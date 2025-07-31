fc30ef90fa285618990d1751f402020ce350b2ad
Sajana Wickramarathna <sajanawickramarathna143@gmail.com>
2025-07-31 15:14:55 +0530

    create contact form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import { Route, Routes } from "react-router-dom";
-import Header from './Components/header'
+import { Routes, Route } from "react-router-dom";
+import Header from './Components/header';
 import Footer from './Components/footer';
 import Home from './pages/Home';
-
+import Contact from './pages/contactus'; // ← import Contact page
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <Header />   
-      <Home />
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} /> {/* ← add contact route */}
+      </Routes>
+      
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
